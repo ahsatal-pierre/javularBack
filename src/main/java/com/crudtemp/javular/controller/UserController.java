4a358116e1407cf6f43d52ad33d1f49970e987d8
ahsatal-pierre <asathal.pierre@gmail.com>
2024-02-14 21:29:06 +0100

    cors origin

--- src/main/java/com/crudtemp/javular/controller/UserController.java
+++ src/main/java/com/crudtemp/javular/controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
