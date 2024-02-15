91d0513a0aaa1445e73931a01b8f007748f1ad9f
ahsatal-pierre <asathal.pierre@gmail.com>
2024-02-15 11:35:18 +0100

    search by name function OK

--- src/main/java/com/crudtemp/javular/controller/UserController.java
+++ src/main/java/com/crudtemp/javular/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crudtemp.javular.model.User;
@@ -35,6 +36,13 @@ public class UserController {
   public User getUserById(@PathVariable Long id) {
     return userRepository.findById(id).get();
   }
+
+  @GetMapping("/search")
+  public List<User> getUsersByName(@RequestParam String name) {
+    return userRepository.findByName(name);
+  }
+  
+
 
   @PostMapping
   public User createUser(@RequestBody User user) {
